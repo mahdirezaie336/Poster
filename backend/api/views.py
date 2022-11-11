e7a24b4db71ddeac3a020695c879a449ea6cbeee
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-11-11 17:28:20 +0330

    Create serializers for post

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,15 @@
-from django.http import JsonResponse
-import django
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from posts.models import Post
 
 
-def api_home(request, *args, **kwargs):
+@api_view(['GET'])
+def api_home(request, *args):
+    """
+    This is the home page for the API
+    """
     body = request.body
     print(body)
     print(args)
     print(request.GET)
-    return JsonResponse({"message": "Hello, world!"})
+    return Response({"message": "Hello, world!"})
--- backend/posts/models.py
+++ backend/posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     id = models.IntegerField(primary_key=True)
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
     email = models.EmailField(max_length=255)
     state = models.CharField(max_length=255)
-    category = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, blank=True, null=True)
