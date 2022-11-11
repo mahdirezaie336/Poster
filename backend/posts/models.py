6be10fc392f54d930e1171c53b5d5f6ecbbec25a
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-11-11 17:13:59 +0330

    Create post model

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    id = models.IntegerField(primary_key=True)
+    description = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    state = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
