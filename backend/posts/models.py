7caa2ec546230a10e919a9ef62f85adcce3c0a7f
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-11-17 22:59:07 +0330

    Add saving image features

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -13,3 +13,9 @@ class Post(models.Model):
     email = models.EmailField(max_length=255)
     state = models.CharField(max_length=255)
     category = models.CharField(max_length=255, blank=True, null=True, choices=CATEGORY_CHOICE, default='P')
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='store/images/', blank=True, null=True)
+    # image_url = models.CharField(max_length=255, blank=True, null=True)
