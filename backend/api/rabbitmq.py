6c54cf51a37c3bc719c484ab2c9b23589e417990
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-11-18 15:20:00 +0330

    Create rabbitmq.py and use in post api

---
+++ backend/api/rabbitmq.py
@@ -0,0 +1,33 @@
+import pika, json
+
+from .secret import AMQP_URL
+
+
+class RabbitMQ:
+
+    def __init__(self, queue_name='hello'):
+        self.queue_name = queue_name
+        self.connection = pika.BlockingConnection(pika.URLParameters(AMQP_URL))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=queue_name)
+
+    def publish(self, message: dict):
+        self.channel.basic_publish(exchange='', routing_key='hello', body=json.dumps(message).encode())
+        print(" [x] Sent %r" % message)
+
+    def receive(self):
+        def callback(ch, method, properties, body):
+            print(" [x] Received %r" % body)
+            return body
+
+        self.channel.basic_consume(queue=self.queue_name, on_message_callback=callback, auto_ack=True)
+
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        try:
+            self.channel.start_consuming()
+        except KeyboardInterrupt:
+            self.connection.close()
+            print(' [*] Connection closed')
+
+    def close(self):
+        self.connection.close()
--- backend/api/views.py
+++ backend/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from posts.models import Post
 from posts.serializers import PostSerializer
+from .rabbitmq import RabbitMQ
 from django.core.files.storage import default_storage
 
 
@@ -24,3 +25,21 @@ class PostViewSet(viewsets.ModelViewSet):
             return Post.objects.filter(id=self.kwargs['pk'])
         # Get all
         return Post.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        # Create a new post
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        # Create post object from request
+        post = Post.objects.get(id=serializer.data['id'])
+
+        # Send the post to the queue
+        queue_data = dict(serializer.data)
+        queue_data['image-name'] = str(post.image)
+        rabbitmq = RabbitMQ()
+        rabbitmq.publish(queue_data)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
