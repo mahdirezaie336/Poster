3709ffbf5b0bab323142f9e011d44fadcd2a0b33
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-11-18 13:00:39 +0330

    Add random name to image fields

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+import uuid
+import os
+
+
+def path_and_rename(instance, filename):
+    """
+    This function is used in the image field to rename the image to a random name.
+    """
+    upload_to = 'store/images/'
+    ext = filename.split('.')[-1]
+    # get filename
+    if instance.pk:
+        filename = '{}.{}'.format(instance.pk, ext)
+    else:
+        # set filename as random string
+        filename = '{}.{}'.format(str(uuid.uuid4()), ext)
+    # return the whole path to the file
+    return os.path.join(upload_to, filename)
 
 
 # Create your models here.
@@ -12,7 +30,7 @@ class Post(models.Model):
     email = models.EmailField(max_length=255)
     state = models.CharField(max_length=255, choices=STATE_CHOICE, default='P')
     category = models.CharField(max_length=255, blank=True, null=True)
-    image = models.ImageField(upload_to='store/images/', blank=True, null=True)
+    image = models.ImageField(upload_to=path_and_rename, blank=True, null=True)
 
 
 # class PostImage(models.Model):
--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -6,7 +6,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'description', 'email', 'state', 'category', 'image']
-        read_only_fields = ['id', 'state']
+        read_only_fields = ['id', 'state', 'category']
         write_only_fields = ['image']
 
 
